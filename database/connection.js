42baf477d658a98ba4e548d0089dbb76ca981d18
franciscoalfar@gmail.com <*Fran2811*>
2023-04-15 18:44:43 -0400

    se crea conexion se importa en index

---
+++ database/connection.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false)
+
+const connection = async() => {
+
+    try {
+        //conexion mediante url a la BD mongo
+
+        await mongoose.connect("mongodb://localhost:27017/social_network");
+        
+
+        //paramtros dentro de obejeto en caso de problemas de conexion
+        //use NewUrlParser:true
+        //useUnifiedTopology:true
+        //useCreateIndex:true
+
+        console.log("Connection success social_network")
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error("The connection has been refused..");
+        
+    }
+
+}
+
+module.exports={
+    connection
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+//importar dependencia de conexion
+const {connection} = require("./database/connection");
+
+console.log("conectado a DB")
+// efectuar conexion a BD
+connection();
+
+
+//crear conexion a servidor de node
+
+//configurar cors
+
+
+//conertir los datos del body a obj js
+
+
+//cargar rutas
+
+//escuchar peticiones http
