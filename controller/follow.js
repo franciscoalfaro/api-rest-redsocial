070a7ec66dc5175aa1a098716ea2dd1fa83ac7d8
franciscoalfar@gmail.com <*Fran2811*>
2023-04-23 21:26:16 -0400

    creacion de model y controlador de follow

--- controller/follow.js
+++ controller/follow.js
@@ -1,3 +1,6 @@
+const Follow = require("../models/follow")
+const User = require("../models/user")
+
 const pruebaFollow = (req, res) => {
     return res.status(200).send({
         message:"mensaje enviado desde controller pruebaFollow"
