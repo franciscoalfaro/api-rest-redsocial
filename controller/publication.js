c13dccedabf1cf013be25f37afa233625d03b282
franciscoalfar@gmail.com <*Fran2811*>
2023-05-17 14:29:07 -0400

    creacion de metodo y schema de publication

--- controller/publication.js
+++ controller/publication.js
@@ -1,8 +1,26 @@
+const Publication = require("../models/publication")
+
+
 const pruebaPublication = (req, res) => {
     return res.status(200).send({
         message:"mensaje enviado desde controller pruebaPublication"
     })
 }
+
+//guardar publicaciones
+
+//obtener una publicacion
+
+//eliminar publicaciones
+
+//listar publicaciones
+
+
+//listar publicaciones de un usuario
+
+//subir ficheros
+
+//devolver archivos multimedia
 
 //
 module.exports = {
