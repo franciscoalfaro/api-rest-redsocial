6615e6bc712f468d5e01f908e4034e694b13d069
franciscoalfar@gmail.com <*Fran2811*>
2023-05-18 22:45:53 -0400

    string de conexion

--- database/connection.js
+++ database/connection.js
@@ -6,9 +6,9 @@ const connection = async() => {
     try {
         //conexion mediante url a la BD mongo
 
-        await mongoose.connect("mongodb://localhost:27017/social_network");
-        
-
+        //await mongoose.connect("mongodb://localhost:27017/social_network");
+        await mongoose.connect("mongodb+srv://franciscoalfar:stpI5QI5cvqcvrmM@cluster1.hru2qfl.mongodb.net/social_network")
+   
         //paramtros dentro de obejeto en caso de problemas de conexion
         //use NewUrlParser:true
         //useUnifiedTopology:true
